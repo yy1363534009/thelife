99a5b1dc2956c7b756f94d246b865fb5d41fe39f
1363534009@qq.com <1363534009@qq.com>
2020-11-25 23:38:44 +0800

    springcloud架构-第四次提交

---
+++ thelife-zuul/src/main/java/com/yuyue/thelife/zuul/security/config/ZuulConfig.java
@@ -0,0 +1,34 @@
+package com.yuyue.thelife.zuul.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @Description: 网关服务配置
+ * @Auther: yuyue
+ * @create: 2020-11-25 23:20:01
+ */
+@Configuration
+public class ZuulConfig {
+
+    /**
+     * 解决前后端分离中的cors跨域问题
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        //↓核心代码
+        corsConfiguration.addExposedHeader("Authorization");
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+
+}
