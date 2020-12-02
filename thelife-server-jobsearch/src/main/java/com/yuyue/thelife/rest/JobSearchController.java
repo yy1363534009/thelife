1a5530d8727ee9198ad5a09adda3adbf52d68cd0
1363534009@qq.com <1363534009@qq.com>
2020-12-03 00:19:18 +0800

    第九次提交

--- thelife-server-jobsearch/src/main/java/com/yuyue/thelife/rest/JobSearchController.java
+++ thelife-server-jobsearch/src/main/java/com/yuyue/thelife/rest/JobSearchController.java
@@ -1,6 +1,8 @@
 package com.yuyue.thelife.rest;
 
 import com.yuyue.thelife.result.JsonRestResponseVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Auther: yuyue
  * @create: 2020-11-23 23:42:16
  */
+@Api(tags = "服务：找活接口")
 @RestController
 @RequestMapping(value = "/jobsearch")
 public class JobSearchController {
@@ -18,6 +21,7 @@ public class JobSearchController {
     @Value("${spring.application.name}")
     private String applicationName;
 
+    @ApiOperation(value = "获取找活列表",notes = "获取找活列表")
     @GetMapping
     public JsonRestResponseVo get() {
         Object object = "获取找工作信息成功" + applicationName;
