f080d36d5f2426db7df79f3d3dd40e0e41e56129
yy1363534009 <yy1363534009@163.com>
2020-12-03 11:35:43 +0800

    mac提交：所有服务需要认证才能访问处理

--- thelife-server-about/src/main/java/com/yuyue/thelife/TheLifeAboutAppRun.java
+++ thelife-server-about/src/main/java/com/yuyue/thelife/TheLifeAboutAppRun.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.context.request.RequestContextListener;
 
 /**
  * @Description: rest接口启动器
@@ -19,11 +17,6 @@ public class TheLifeAboutAppRun {
 
     public static void main(String[] args) {
         SpringApplication.run(TheLifeAboutAppRun.class, args);
-    }
-
-    @Bean
-    public RequestContextListener requestContextListener(){
-        return new RequestContextListener();
     }
 
 }
